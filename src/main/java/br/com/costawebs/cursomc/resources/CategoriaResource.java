15c63112b33105b89459443dad88c1c259b9bf3f
costaevangelista <costaevangelista@live.com>
2018-04-13 14:28:13 -0400

    Criando Repositories e Services para Categoria

--- src/main/java/br/com/costawebs/cursomc/resources/CategoriaResource.java
+++ src/main/java/br/com/costawebs/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,26 @@
 package br.com.costawebs.cursomc.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.costawebs.cursomc.domain.Categoria;
+import br.com.costawebs.cursomc.services.CategoriaService;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
-	@GetMapping
-	public String index() {
-		return "Index de resource categoria";
+	@Autowired
+	private CategoriaService service;
+	
+	@GetMapping(value="/{id}")
+	public ResponseEntity<?> find(@PathVariable Long id) {
+		Categoria obj = service.find(id);	
+		return ResponseEntity.ok().body(obj);		
 	}
 
 }
