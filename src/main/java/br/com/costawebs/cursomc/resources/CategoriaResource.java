6baabb47f5679ef38f9816bd18c92239b12c3974
costaevangelista <costaevangelista@live.com>
2018-04-14 10:37:44 -0400

    Listando Categoria Paginação com parametros opcionais

--- src/main/java/br/com/costawebs/cursomc/resources/CategoriaResource.java
+++ src/main/java/br/com/costawebs/cursomc/resources/CategoriaResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -21,45 +23,55 @@ import br.com.costawebs.cursomc.dto.CategoriaDTO;
 import br.com.costawebs.cursomc.services.CategoriaService;
 
 @RestController
-@RequestMapping(value="/categorias")
+@RequestMapping(value = "/categorias")
 public class CategoriaResource {
-	
+
 	@Autowired
 	private CategoriaService service;
-	
+
 	@GetMapping
 	public ResponseEntity<List<CategoriaDTO>> findAll() {
 		List<Categoria> list = service.findAll();
 		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
-		return ResponseEntity.ok().body(listDto);		
+		return ResponseEntity.ok().body(listDto);
 	}
-	
-	@GetMapping(value="/{id}")
+
+	@GetMapping(value = "/page")
+	public ResponseEntity<Page<CategoriaDTO>> findPage(@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderby,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction) {
+		Page<Categoria> list = service.findPage(page, linesPerPage, orderby, direction);
+		Page<CategoriaDTO> listDto = list.map(obj -> new CategoriaDTO(obj));
+		return ResponseEntity.ok().body(listDto);
+	}
+
+	@GetMapping(value = "/{id}")
 	public ResponseEntity<Categoria> find(@PathVariable Long id) {
-		Categoria obj = service.find(id);	
-		return ResponseEntity.ok().body(obj);		
+		Categoria obj = service.find(id);
+		return ResponseEntity.ok().body(obj);
 	}
-	
+
 	@PostMapping
 	public ResponseEntity<?> create(@RequestBody Categoria categoria) {
-		categoria = service.create(categoria);	
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
-				.path("/{id}").buildAndExpand(categoria.getId()).toUri();
-		
+		categoria = service.create(categoria);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(categoria.getId())
+				.toUri();
+
 		return ResponseEntity.created(uri).build();
 	}
-	
-	@PutMapping(value="/{id}")
+
+	@PutMapping(value = "/{id}")
 	public ResponseEntity<?> update(@RequestBody Categoria categoria, @PathVariable Long id) {
 		categoria.setId(id);
-		categoria = service.update(categoria);	
-		return ResponseEntity.noContent().build();	
+		categoria = service.update(categoria);
+		return ResponseEntity.noContent().build();
 	}
-	
-	@DeleteMapping(value="/{id}")
-	public ResponseEntity<?> delete( @PathVariable Long id) {
-		service.delete(id);	
-		return ResponseEntity.noContent().build();	
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<?> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 
 }
--- src/main/java/br/com/costawebs/cursomc/services/CategoriaService.java
+++ src/main/java/br/com/costawebs/cursomc/services/CategoriaService.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import br.com.costawebs.cursomc.domain.Categoria;
@@ -17,6 +20,10 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repository;
 
+	public List<Categoria> findAll() {
+		return repository.findAll();
+	}
+
 	public Categoria find(Long id) {
 		Categoria obj = repository.findOne(id);
 		if (obj == null) {
@@ -25,7 +32,7 @@ public class CategoriaService {
 		}
 		return obj;
 	}
-	
+
 	public Categoria create(Categoria categoria) {
 		categoria.setId(null);
 		return repository.save(categoria);
@@ -43,11 +50,12 @@ public class CategoriaService {
 		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos cadastrados");
 		}
-		
-		
+
 	}
 
-	public List<Categoria> findAll() {
-		return repository.findAll();
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderby, String direction) {
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderby);
+		return repository.findAll(pageRequest);
 	}
+
 }
