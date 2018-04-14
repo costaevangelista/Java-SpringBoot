bed2ece595d92b96979144b42ec48fac9a860b10
costaevangelista <costaevangelista@live.com>
2018-04-13 23:29:33 -0400

    Deletando uma Categoria com DELETE and DataIntegrityException

--- src/main/java/br/com/costawebs/cursomc/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/br/com/costawebs/cursomc/resources/exception/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import br.com.costawebs.cursomc.services.exceptions.DataIntegrityException;
 import br.com.costawebs.cursomc.services.exceptions.ObjectNotFoundException;
 
 @ControllerAdvice
@@ -16,6 +17,13 @@ public class ResourceExceptionHandler {
 	public ResponseEntity<StandarError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
 		
 		StandarError err = new StandarError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
+	
+	@ExceptionHandler(DataIntegrityException.class)
+	public ResponseEntity<StandarError> aataIntegrity(DataIntegrityException e, HttpServletRequest request){
+		
+		StandarError err = new StandarError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
 	}
 
--- src/main/java/br/com/costawebs/cursomc/services/CategoriaService.java
+++ src/main/java/br/com/costawebs/cursomc/services/CategoriaService.java
@@ -1,10 +1,12 @@
 package br.com.costawebs.cursomc.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import br.com.costawebs.cursomc.domain.Categoria;
 import br.com.costawebs.cursomc.repositories.CategoriaRepository;
+import br.com.costawebs.cursomc.services.exceptions.DataIntegrityException;
 import br.com.costawebs.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -30,5 +32,16 @@ public class CategoriaService {
 	public Categoria update(Categoria categoria) {
 		find(categoria.getId());
 		return repository.save(categoria);
+	}
+
+	public void delete(Long id) {
+		find(id);
+		try {
+			repository.delete(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos cadastrados");
+		}
+		
+		
 	}
 }
