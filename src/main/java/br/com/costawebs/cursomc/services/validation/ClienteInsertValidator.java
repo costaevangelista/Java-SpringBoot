476783165e0215887c5dc2e947464f042de9f4a6
costaevangelista <costaevangelista@live.com>
2018-04-16 12:06:17 -0400

    Validação customizada de Email na insercao de Cliente

--- src/main/java/br/com/costawebs/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/br/com/costawebs/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,16 +6,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.com.costawebs.cursomc.domain.Cliente;
 import br.com.costawebs.cursomc.domain.enums.TipoCliente;
 import br.com.costawebs.cursomc.dto.ClienteNewDTO;
+import br.com.costawebs.cursomc.repositories.ClienteRepository;
 import br.com.costawebs.cursomc.resources.exception.FieldMessage;
 import br.com.costawebs.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
 
-	/*
-	 * @Autowired private ClienteRepository clienteRepository;
-	 */
+	@Autowired
+	private ClienteRepository clienteRepository;
 
 	@Override
 	public void initialize(ClienteInsert ann) {
@@ -35,10 +38,10 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
 		}
 
-		/*
-		 * Cliente aux = clienteRepository.findByEmail(objDto.getEmail()); if (aux !=
-		 * null) { list.add(new FieldMessage("email", "Email já existente")); }
-		 */
+		Cliente aux = clienteRepository.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
+		}
 
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
