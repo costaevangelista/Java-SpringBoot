8e0384b9296e4c834e0079350a471bf90131fa43
costaevangelista <costaevangelista@live.com>
2018-04-16 11:55:29 -0400

    Validação customizada de CPF ou CNPJ

---
+++ src/main/java/br/com/costawebs/cursomc/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,50 @@
+package br.com.costawebs.cursomc.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import br.com.costawebs.cursomc.domain.enums.TipoCliente;
+import br.com.costawebs.cursomc.dto.ClienteNewDTO;
+import br.com.costawebs.cursomc.resources.exception.FieldMessage;
+import br.com.costawebs.cursomc.services.validation.utils.BR;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+	/*
+	 * @Autowired private ClienteRepository clienteRepository;
+	 */
+
+	@Override
+	public void initialize(ClienteInsert ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+		List<FieldMessage> list = new ArrayList<>();
+
+		// inclua os testes aqui, inserindo erros na lista
+
+		if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
+		}
+
+		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+
+		/*
+		 * Cliente aux = clienteRepository.findByEmail(objDto.getEmail()); if (aux !=
+		 * null) { list.add(new FieldMessage("email", "Email já existente")); }
+		 */
+
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
